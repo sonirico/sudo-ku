52cad0be8e180464216317ecef3489769c9eb0c1
sonirico <marsanben92@hotmail.com>
2015-07-05 22:16:08 +0200

    Backtracking sudoku resolver implementation
    Create a Java programm able to solve sudoku puzzles. It shows how
    many solutions are possible as well as them.

---
+++ SudokuMaker.java
@@ -0,0 +1,77 @@
+public class SudokuMaker {
+	
+	public static final int [] numbers = new int [] {
+		1, 2, 3, 4, 5, 6, 7, 8, 9
+	};
+	
+	public static int nSolutions = 0;
+
+	public static void main (String args[]){
+		int [][] s = new int [][]{
+				{1, 0, 0, 	0, 0, 0,	0, 0, 0},
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0},
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0},
+				
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0},
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0},
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0},
+				
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0},
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0},
+				{0, 0, 0, 	0, 0, 0,	0, 0, 0}
+		};
+        Sudoku sudoku = new Sudoku(s);
+        
+        System.out.println("Processing this sudoku ...");
+        
+        //for (int i = 0; i < 9; i++)
+		//	for (int j = 0; j < 9; j++)
+		//		sudoku.setCell(i, j, (i*3 + i/3 + j) % (9) + 1);
+        sudoku.print();
+        
+        backtracking(sudoku);
+        System.out.println(nSolutions + " found");
+    }
+	
+	private static boolean backtracking(Sudoku sk){
+
+		
+		if (!sk.hasZeros()) {
+			sk.print();
+			nSolutions++;
+			return true;
+		} else { 
+		
+			int r = -1, c= -1;
+			boolean found = false;
+			
+			for (r = 0 ; r < sk.ROWS; r++){
+				for ( c = 0; c < sk.COLS ; c++){
+					if (sk.emptyCell(r, c)){
+						found = true;
+						break;
+					}
+				}
+				if (found )break;
+			}
+			
+			for ( int n : numbers ){
+				
+				if (sk.looksPromising(r, c, n)){
+					sk.setCell(r, c, n);
+					
+					if ( backtracking(sk) ){
+						continue;//return true;
+					}
+					else{
+						sk.setCell(r, c, 0);
+					}
+				}
+				
+			}
+			
+			return false;
+		}
+	}
+}
+
