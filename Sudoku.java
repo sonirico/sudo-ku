417d28ce0b659465ed5439a7f241f1194c0805fa
sonirico <marsanben92@hotmail.com>
2015-09-03 18:13:04 +0200

    Clean code

--- Sudoku.java
+++ Sudoku.java
@@ -1,144 +1,144 @@
 public class Sudoku {
-	
+
     public static final int ROWS = 9, COLS = 9;
     private int [][] cells;
 
     public Sudoku(){
         this.cells = new int [ROWS][COLS];
         for(int r = 0; r < this.cells.length; r++){
-			for(int c = 0; c < this.cells[r].length; c++){
-				this.cells[r][c] = 0;
-			}
-		}
+            for(int c = 0; c < this.cells[r].length; c++){
+                this.cells[r][c] = 0;
+            }
+        }
     }
-    
-    
+
+
     public Sudoku(int [][] newCells){
         this.cells = newCells;
     }
-    
-    public int[][] get(){
-		return this.cells;
-	}
-	
-	public void set(int [][] newCells){
-		this.cells = newCells;
-	}
-	
-	
-	// ---
-	
-	public boolean emptyCell(int r, int c){
-		//TODO: check bounds
-		return this.cells[r][c] == 0;
-	}
-	
-	
-	public boolean inRow(int row, int n){
-		if (row >= ROWS || row < 0 ) throw new Error("Row out of bounds: " + row);
-		
-		for(int c = 0; c < this.cells[row].length; c++){
-			if ( this.cells[row][c] == n){
-				//System.out.println("Found in row. ("+row+", "+c+") = " + n);
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public boolean inCol(int col, int n){
-		if (col >= COLS || col < 0 ) throw new Error("Col out of bounds: " + col);
-		
-		for(int r = 0; r < this.cells.length; r++){
-			if ( this.cells[r][col] == n){
-				//System.out.println("Found in col. ("+r+", "+col+") = " + n);
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public boolean inDiagons(int n){
-		
-		for(int r = 0; r < this.cells.length; r++){
-			if ( this.cells[r][r] == n){
-				return true;
-			}
-			else if (this.cells[this.cells.length - r - 1][r] == n ){
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	
-	
-	public boolean inQuadrant(int row, int col, int n){
-		int rmin = (row / 3) * 3;
-		int rmax = rmin + 3;
-		int cmin = (col / 3) * 3;
-		int cmax = cmin + 3;
-		for (int r = rmin; r < rmax; r++){
-			for (int c = cmin; c < cmax; c++){
-				if (this.cells[r][c] == n ){
-					//System.out.println("Found in row. ("+r+", "+c+") = " + n);
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	public void setCell(int r, int c, int n){
-		//TODO: Check bounds
-		this.cells[r][c] = n;
-		//System.out.println("Set " + n + "in ( " + r + ", " + c + " )");
-	}
-	
-	// ---
-	
-	public boolean hasZeros(){
-		int nZeros = 0;
-        for(int r = 0; r < this.cells.length; r++){
-			for(int c = 0; c < this.cells[r].length; c++){
-				if (this.cells[r][c] == 0){
-					return true;
-				}
-			}
-		}
-		return false;
-	}
-	
-	public boolean looksPromising(int r, int c, int num){
-		return !this.inCol(c, num) && !this.inRow(r, num) && !this.inQuadrant(r, c, num) ;//&& !this.inDiagons(num);		
-	}
-	
 
-	
-	
-	public void print (){
-		for(int r = 0; r < this.cells.length; r++){
-			if (r % 3 == 0){
-				System.out.print("\n");
-			}
-			for(int c = 0; c < this.cells[r].length; c++){
-				if (c % 3 == 0){
-					System.out.print("\t");
-				}
-				if (this.cells[r][c] > 0){
-					System.out.print("|" + this.cells[r][c]);
-				} else {
-					System.out.print("|·");
-				}
-			}
-			System.out.print("|\n");
-			
-		}		
-		System.out.print("--------------------------------------\n");
-	}
-	
+    public int[][] get(){
+        return this.cells;
+    }
+
+    public void set(int [][] newCells){
+        this.cells = newCells;
+    }
+
+
+    // ---
+
+    public boolean emptyCell(int r, int c){
+        //TODO: check bounds
+        return this.cells[r][c] == 0;
+    }
+
+
+    public boolean inRow(int row, int n){
+        if (row >= ROWS || row < 0 ) throw new Error("Row out of bounds: " + row);
+
+        for(int c = 0; c < this.cells[row].length; c++){
+            if ( this.cells[row][c] == n){
+                //System.out.println("Found in row. ("+row+", "+c+") = " + n);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean inCol(int col, int n){
+        if (col >= COLS || col < 0 ) throw new Error("Col out of bounds: " + col);
+
+        for(int r = 0; r < this.cells.length; r++){
+            if ( this.cells[r][col] == n){
+                //System.out.println("Found in col. ("+r+", "+col+") = " + n);
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public boolean inDiagons(int n){
+
+        for(int r = 0; r < this.cells.length; r++){
+            if ( this.cells[r][r] == n){
+                return true;
+            }
+            else if (this.cells[this.cells.length - r - 1][r] == n ){
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+
+
+    public boolean inQuadrant(int row, int col, int n){
+        int rmin = (row / 3) * 3;
+        int rmax = rmin + 3;
+        int cmin = (col / 3) * 3;
+        int cmax = cmin + 3;
+        for (int r = rmin; r < rmax; r++){
+            for (int c = cmin; c < cmax; c++){
+                if (this.cells[r][c] == n ){
+                    //System.out.println("Found in row. ("+r+", "+c+") = " + n);
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void setCell(int r, int c, int n){
+        //TODO: Check bounds
+        this.cells[r][c] = n;
+        //System.out.println("Set " + n + "in ( " + r + ", " + c + " )");
+    }
+
+    // ---
+
+    public boolean hasZeros(){
+        int nZeros = 0;
+        for(int r = 0; r < this.cells.length; r++){
+            for(int c = 0; c < this.cells[r].length; c++){
+                if (this.cells[r][c] == 0){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    public boolean looksPromising(int r, int c, int num){
+        return !this.inCol(c, num) && !this.inRow(r, num) && !this.inQuadrant(r, c, num) ;//&& !this.inDiagons(num);		
+    }
+
+
+
+
+    public void print (){
+        for(int r = 0; r < this.cells.length; r++){
+            if (r % 3 == 0){
+                System.out.print("\n");
+            }
+            for(int c = 0; c < this.cells[r].length; c++){
+                if (c % 3 == 0){
+                    System.out.print("\t");
+                }
+                if (this.cells[r][c] > 0){
+                    System.out.print("|" + this.cells[r][c]);
+                } else {
+                    System.out.print("|·");
+                }
+            }
+            System.out.print("|\n");
+
+        }		
+        System.out.print("--------------------------------------\n");
+    }
+
 
 }
--- SudokuMaker.java
+++ SudokuMaker.java
@@ -1,76 +1,76 @@
 public class SudokuMaker {
-	
-	public static final int [] numbers = new int [] {
-		1, 2, 3, 4, 5, 6, 7, 8, 9
-	};
-	
-	public static int nSolutions = 0;
 
-	public static void main (String args[]){
-		int [][] s = new int [][]{
-				{9, 0, 6,   0, 7, 0,    4, 0, 3},
-				{0, 0, 0,   4, 0, 0,    2, 0, 0},
-				{0, 7, 0,   0, 2, 3,	0, 1, 0},
-				
-				{5, 0, 0,   0, 0, 0,    1, 0, 0},
-				{0, 4, 0,   2, 0, 8,    0, 6, 0},
-				{0, 0, 3,   0, 0, 0,    0, 0, 5},
-				
-				{0, 3, 0,   7, 0, 0,    0, 5, 0},
-				{0, 0, 7,   0, 0, 5,    0, 0, 0},
-				{4, 0, 5,   0, 1, 0,    7, 0, 8}
-		};
+    public static final int [] numbers = new int [] {
+        1, 2, 3, 4, 5, 6, 7, 8, 9
+    };
+
+    public static int nSolutions = 0;
+
+    public static void main (String args[]){
+        int [][] s = new int [][]{
+                {9, 0, 6,   0, 7, 0,    4, 0, 3},
+                {0, 0, 0,   4, 0, 0,    2, 0, 0},
+                {0, 7, 0,   0, 2, 3,	0, 1, 0},
+
+                {5, 0, 0,   0, 0, 0,    1, 0, 0},
+                {0, 4, 0,   2, 0, 8,    0, 6, 0},
+                {0, 0, 3,   0, 0, 0,    0, 0, 5},
+
+                {0, 3, 0,   7, 0, 0,    0, 5, 0},
+                {0, 0, 7,   0, 0, 5,    0, 0, 0},
+                {4, 0, 5,   0, 1, 0,    7, 0, 8}
+        };
         Sudoku sudoku = new Sudoku(s);
-        
+
         System.out.println("Processing this sudoku ...");
-        
+
         sudoku.print();
-        
+
         backtracking(sudoku);
         System.out.println(nSolutions + " found");
     }
-	
-	private static boolean backtracking(Sudoku sk){
 
-		
-		if (!sk.hasZeros()) {
-			sk.print();
-			nSolutions++;
-			return false;
-		} else { 
-		
-			int r = -1, c= -1;
-			
-			
-			for (r = 0 ; r < sk.ROWS; r++){
-				boolean found = false;
-				for ( c = 0; c < sk.COLS ; c++){
-					if (sk.emptyCell(r, c)){
-						found = true;
-						break;
-					}
-				}
-				if (found )break;
-			}
-			
-			for ( int n : numbers ){
-				
-				if (sk.looksPromising(r, c, n)){
-					sk.setCell(r, c, n);
-					//backtracking(sk);
-					
-					if ( backtracking(sk) ){
-						//continue;//return true;
-					}
-					else{
-						sk.setCell(r, c, 0);
-					}
-				}
-				
-			}
-			
-			return false;
-		}
-	}
+    private static boolean backtracking(Sudoku sk){
+
+
+        if (!sk.hasZeros()) {
+            sk.print();
+            nSolutions++;
+            return false;
+        } else { 
+
+            int r = -1, c= -1;
+
+
+            for (r = 0 ; r < sk.ROWS; r++){
+                boolean found = false;
+                for ( c = 0; c < sk.COLS ; c++){
+                    if (sk.emptyCell(r, c)){
+                        found = true;
+                        break;
+                    }
+                }
+                if (found )break;
+            }
+
+            for ( int n : numbers ){
+
+                if (sk.looksPromising(r, c, n)){
+                    sk.setCell(r, c, n);
+                    //backtracking(sk);
+
+                    if ( backtracking(sk) ){
+                        //continue;//return true;
+                    }
+                    else{
+                        sk.setCell(r, c, 0);
+                    }
+                }
+
+            }
+
+            return false;
+        }
+    }
 }
 
